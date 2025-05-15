979676faa076fe814227ed11ed933b1f34c0c46e
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-05-15 19:20:05 +0530

    h1

--- To-Do List/src/App.jsx
+++ To-Do List/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   
   return (
     <>
+     <div className='container'>
+      <h1>To-List Project</h1>
+
+
+     </div>
      
     </>
   )
